f88c258d3993f170ae620dba339f883619bc50d0
Sebastian Brizuela <sebita29@gmail.com>
2026-05-23 22:56:40 -0300

    Delete Cadenas directory

--- Cadenas/palindromo.py
+++
@@ -1,6 +0,0 @@
-def es_palindromo(palabra):
-    """Recibe un texto y devuelve True si se lee igual al derecho qu al revéz y False en caso contrario"""
-
-    palabra = "".join([caracter for caracter in palabra if caracter.isalpha()])
-    palabra_invertida = "".join([caracter for caracter in palabra if caracter.isalpha()][::-1])
-    return palabra.upper() == palabra_invertida.upper()
